c51d501d19ee7a124931706bfe71bcb960df3ed0
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2024-07-16 20:24:38 +0500

    onboarding images fix

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      home:Scaffold(
-        body:
-       // signup()
-      //  login()
-        //profileedit()
-    //   settings()
-       bottomNB()
-        //onboarding()
-       // page1()
-      )
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body:
+                // signup()
+                //  login()
+                //profileedit()
+                //   settings()
+                //  bottomNB()
+                onboarding()
+            // page1()
+            ));
   }
 }
--- lib/views/onboarding_screen/onboarding.dart
+++ lib/views/onboarding_screen/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_task_app/views/bnb/bnb.dart';
 
 class onboarding extends StatefulWidget {
   const onboarding({super.key});
@@ -17,20 +18,20 @@ class _onboardingState extends State<onboarding> {
       "title4": "for your",
       "title5": "workflows."
     },
-     {
-      "image": 'assets/images/image11.png',
+    {
+      "image": 'assets/images/Image11.png',
       "title": 'Task Management',
       "title2": "Work more ",
       "title3": "structured  ",
-        "title4": "and",
+      "title4": "and",
       "title5": "organized "
     },
-     {
-      "image": 'assets/images/image1.png',
+    {
+      "image": 'assets/images/Image1.png',
       "title": 'Task Management',
       "title2": "Manage your",
       "title3": "task",
-        "title4": "quickly for",
+      "title4": "quickly for",
       "title5": "results"
     }
   ];
@@ -39,86 +40,173 @@ class _onboardingState extends State<onboarding> {
   Color otherColor = Color(0xFF19173D);
   PageController pageController = PageController();
 
-int currentPage =0;
-  onChanged(int index){
+  int currentPage = 0;
+  onChanged(int index) {
     setState(() {
       currentPage = index;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: otherColor,
-      body: Stack(children: [
-        PageView.builder(
-        scrollDirection: Axis.horizontal,
-        controller: pageController,
-        itemCount: onboarding.length,
-    //    onPageChanged: ,
-        itemBuilder: (context, index) {
-          return SingleChildScrollView(scrollDirection: Axis.vertical,
-            child:Column(children: [
-              Stack(
-                children:[ Container(height: 350,width: 400,
-                  child: Image.asset(onboarding[index]['image'],fit: BoxFit.cover,)),
-                   Padding(
-                     padding: const EdgeInsets.only(left: 15,right: 15,top: 15),
-                     child: Column(children: [
-                              Align(alignment: Alignment.bottomRight,
-                                child: Image.asset('assets/images/image3.png')),
-                       Align(alignment: Alignment.bottomLeft,
-                        child: Image.asset('assets/images/image2.png')),
-                         SizedBox(height: 10,),
-                            Align(alignment: Alignment.bottomRight,
-                              child: Container(height: 120,width: 200,
-                                child: Image.asset('assets/images/image1(2).png',fit: BoxFit.cover,),)),
-                            ],),),
-            ]),
-             // SizedBox(height: 3,),
-              Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
+        children: [
+          PageView.builder(
+            scrollDirection: Axis.horizontal,
+            controller: pageController,
+            itemCount: onboarding.length,
+            //    onPageChanged: ,
+            itemBuilder: (context, index) {
+              return SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
                   children: [
-                   Text(onboarding[index]['title'],style: TextStyle(color: Colors.blue,fontSize: 20),),
-                 Text(onboarding[index]['title2'],style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 30),),
-                 Row(
-                   children: [
-                     Text(onboarding[index]['title3'],style: TextStyle(color: Colors.blue,fontWeight: FontWeight.w600,fontSize: 30),),
-                         Text(onboarding[index]['title4'],style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 30),),
-                   ],),
-                 Text(onboarding[index]['title5'],style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 30),),
-                SizedBox(height: 7,),
-                   
-                     Row(children:
-            List.generate(onboarding.length, (index){
-              return AnimatedContainer(duration: Duration(milliseconds: 200,),height: 10,
-              width: (index == currentPage) ? 15 : 10,
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),
-              color: (index == currentPage) ? Colors.deepOrange : Colors.grey),
-              );
-            })),
-           SizedBox(height: 10,),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Skip",style: TextStyle(color: Colors.white,fontSize: 15),),
+                    Stack(children: [
+                      Container(
+                          height: 350,
+                          width: 400,
+                          child: Image.asset(
+                            onboarding[index]['image'],
+                            fit: BoxFit.cover,
+                          )),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 15, right: 15, top: 15),
+                        child: Column(
+                          children: [
+                            Align(
+                                alignment: Alignment.bottomRight,
+                                child: Image.asset('assets/images/Image3.png')),
+                            Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Image.asset('assets/images/Image2.png')),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Align(
+                                alignment: Alignment.bottomRight,
+                                child: Container(
+                                  height: 120,
+                                  width: 200,
+                                  child: Image.asset(
+                                    'assets/images/Image1(2).png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ]),
+                    // SizedBox(height: 3,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            onboarding[index]['title'],
+                            style: TextStyle(color: Colors.blue, fontSize: 20),
+                          ),
+                          Text(
+                            onboarding[index]['title2'],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 30),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                onboarding[index]['title3'],
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 30),
+                              ),
+                              Text(
+                                onboarding[index]['title4'],
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 30),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            onboarding[index]['title5'],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 30),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Row(
+                              children:
+                                  List.generate(onboarding.length, (index) {
+                            return AnimatedContainer(
+                              duration: Duration(
+                                milliseconds: 200,
+                              ),
+                              height: 10,
+                              width: (index == currentPage) ? 15 : 10,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: (index == currentPage)
+                                      ? Colors.deepOrange
+                                      : Colors.grey),
+                            );
+                          })),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    bottomNB()));
+                                      },
+                                      child: Text(
+                                        "Skip",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 15),
+                                      )),
+                                ],
+                              ),
+
+                              //const EdgeInsets.only(bottom: 20),
+                              Container(
+                                  height: 136,
+                                  width: 70,
+                                  child: Image.asset(
+                                    'assets/images/NextButton.png',
+                                    fit: BoxFit.cover,
+                                  )),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
-                 
-              //const EdgeInsets.only(bottom: 20),
-              Container(height: 136,width: 70,
-                child: Image.asset('assets/images/NextButton.png',fit: BoxFit.cover,)),
-            
-              ],),],),)
-            ],),
-          );
-        },
-         ),
-      ],),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
